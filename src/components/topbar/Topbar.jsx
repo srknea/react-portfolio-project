096efc11f1548ff46c57193f59aff501e3ef98ab
Serkan ISIK <dehnas79@gmail.com>
2022-08-25 16:16:36 +0300

    Material-UI Icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
     <div className="topbar">
-     
-    </div>
+        <div className="wrapper">
+            <div className="left">
+              <a href="#intro" className="logo">genius.</a>
+              <div className="itemContainer">
+                <Person className="icon"/>
+                <span>+44 924 12 73</span>
+              </div>
+              <div className="itemContainer">
+                <Mail className="icon"/>
+                <span>safak@genius.com</span>
+              </div>
+            </div>
+            
+            <div className="right">
+              
+            </div>
+        </div>
+    </div> 
   )
 }
