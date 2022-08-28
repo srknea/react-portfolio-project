07e120e40bc5cef48e7d0cc11577f1aea7745526
Serkan ISIK <dehnas79@gmail.com>
2022-08-28 12:31:08 +0300

    introduction page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-    
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'am</h2>
+          <h1>Serkan ISIK</h1>
+          <h3>Senior Student<span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
