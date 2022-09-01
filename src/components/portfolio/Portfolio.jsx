5f13692308d134ea50bd80103945f7f138a30447
Serkan ISIK <dehnas79@gmail.com>
2022-09-01 11:43:39 +0300

    dynamic data with useEffect hook

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
+import{
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
 
   const [selected,setSelected] = useState("featured")
+  const [data,setData] = useState([])
+  
+  
 
   const list = [
     {
@@ -29,6 +39,30 @@ export default function Portfolio() {
     },
   ];
 
+  useEffect(()=>{
+
+    switch(selected){
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+
+  },[selected])
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -43,30 +77,15 @@ export default function Portfolio() {
           ))}
       </ul>
       <div className="container">
+        {data.map((d) => (
           <div className="item">
-            <img src="https://source.unsplash.com/200x100" alt="" />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://source.unsplash.com/200x100" alt="" />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://source.unsplash.com/200x100" alt="" />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://source.unsplash.com/200x100" alt="" />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://source.unsplash.com/200x100" alt="" />
-            <h3>Banking App</h3>
-          </div>
-          <div className="item">
-            <img src="https://source.unsplash.com/200x100" alt="" />
-            <h3>Banking App</h3>
-          </div>
+          <img
+           src={d.img} 
+           alt="" 
+           />
+          <h3>{d.title}</h3>
+        </div>
+        ))}
       </div>
     </div>
   )
